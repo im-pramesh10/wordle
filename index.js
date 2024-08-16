f3782a700e1fc97fbc092f24b2e01b8a7e35c299
Pramesh Mahat <cruisepramesh@gmail.com>
2024-08-17 02:04:15 +0545

    handle cell selection

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import {wordlist} from "./wordlist.js";
 class Game {
     constructor() {
         this.word = wordlist[Math.floor(Math.random() * wordlist.length)];
-        this.row = 1;
+        this.currentRow = 0;
+        this.activeCell = 0;
         this.initializeBoard();
     }
     initializeBoard() {
@@ -23,12 +24,28 @@ class Game {
             row.className = "row";
             for (let j = 0; j < this.board[i].length; j++) {
                 const cell = document.createElement("div");
-                cell.id =(`${i}-${j}`);
+                cell.id = (`${i}-${j}`);
                 cell.className = "cell";
-                cell.innerText = `${i}-${j}`;
+                if (j === this.activeCell && i === this.currentRow) {
+                    cell.className = "cell active";
+                }
+                cell.innerText = this.board[i][j];
+                cell.addEventListener("click", () => {
+                    this.handleCellClick(i, j);
+                })
                 row.appendChild(cell);
             }
             board.appendChild(row);
+        }
+    }
+
+    handleCellClick(row, col) {
+        if (row === this.currentRow) {
+            const currentActiveCell = document.getElementById(`${this.currentRow}-${this.activeCell}`);
+            currentActiveCell.className = "cell";
+            const cell = document.getElementById(`${row}-${col}`);
+            cell.className = "cell active";
+            this.activeCell = col;
         }
     }
     start() {
